54a22c8d01506011199ecbd265ba8a61cd6b786e
USENGROUP/kot-sasaki <kot-sasaki@usen-next.jp>
2023-06-02 17:06:59 +0900

    add data

--- peke2.php
+++ peke2.php
@@ -6,12 +6,16 @@ $response = file_get_contents($url);
 //レスポンスデータは JSON 形式なので、デコードして連想配列にする
 $data = json_decode($response , true);
 print("<pre>");
-var_dump($data);
-print("</pre>");
-print("<pre>");
 var_dump($data["results"]);
+$name = array();
+$pokeArray = array();
 foreach($data["results"] as $key => $value){
-    var_dump($value["name"]);
+    // array_push($name,$value["name"]);
+    // array_push($pokeArray,$value["url"]);
+    echo $value["types"][0]["type"]["name"]."<br>";
+    echo $value["name"]."<br>";
+    echo $value["height"]."<br>";
+    echo $value["weight"]."<br>";
 }
 print("</pre>")
 
