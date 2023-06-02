0b8a28e000aed4a25dafbbedf31d13aa1145bfc8
USENGROUP/kot-sasaki <kot-sasaki@usen-next.jp>
2023-06-02 17:34:20 +0900

    display poke

--- peke2.php
+++ peke2.php
@@ -5,18 +5,29 @@ $url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0';
 $response = file_get_contents($url);
 //レスポンスデータは JSON 形式なので、デコードして連想配列にする
 $data = json_decode($response , true);
-print("<pre>");
-var_dump($data["results"]);
-$name = array();
-$pokeArray = array();
-foreach($data["results"] as $key => $value){
-    // array_push($name,$value["name"]);
-    // array_push($pokeArray,$value["url"]);
-    echo $value["types"][0]["type"]["name"]."<br>";
-    echo $value["name"]."<br>";
-    echo $value["height"]."<br>";
-    echo $value["weight"]."<br>";
-}
-print("</pre>")
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        foreach($data["results"] as $key => $value){
+            $url2 = $value["url"];
+            $response2 = file_get_contents($url2);
+        
+            $data2 = json_decode($response2 , true);
+            // array_push($name,$value["name"]);
+            // array_push($pokeArray,$value["url"]); ?>
+            <img src="<?= $data2['sprites']['front_default'] ?>"><br>
+            名前：<?= $value["name"]; ?><br>
+            タイプ：<?= $data2["types"][0]["type"]["name"]; ?><br>
+            身長：<?= $data2["height"]; ?><br>
+            体重：<?= $data2["weight"]; ?><br>
+        <?php } ?>
+</body>
+</html>
