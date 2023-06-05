b23969050dcdcbb7fd4064434ca48eeaadfc82a9
USENGROUP/kot-sasaki <kot-sasaki@usen-next.jp>
2023-06-05 13:59:15 +0900

    fix_paging

--- peke2.php
+++ peke2.php
@@ -1,7 +1,18 @@
 <?php
+if(empty($_GET)){
+    $offset = 0;
+}
+else{
+    if($_GET["page"] == "次へ"){
+        $offset = 10 + $_GET["back_off"];
+    }
+    else if($_GET["page"] == "前へ"){
+        $offset =  $_GET["back_off"] - 10;
+    }
+}
 
 /**PokeAPI のデータを取得する (URL 末尾の数字はポケモン図鑑の ID) */
-$url = 'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0';
+$url = "https://pokeapi.co/api/v2/pokemon/?limit=10&offset={$offset}";
 $response = file_get_contents($url);
 //レスポンスデータは JSON 形式なので、デコードして連想配列にする
 $data = json_decode($response , true);
@@ -16,25 +27,12 @@ $data = json_decode($response , true);
 </head>
 <body>
     <?php
-        define('MAX','10');
 
-        $poke_num = count($data["results"]);
-        $max_page = ceil($poke_num / MAX);
-
-        if(!isset($_GET['page_id'])){
-            $now = 1;
-        }else{
-            $now = $_GET['page_id'];
-        }
-        
-        $start_no = ($now - 1) * MAX;
-        
-        $disp_data = array_slice($data["results"], $start_no, MAX, true);
-
-        foreach($disp_data as $key => $value){
+        foreach($data["results"] as $key => $value){
             // print("<pre>");
-            // var_dump($disp_data);
+            // var_dump($data["results"]);
             // print("</pre>");
+
             $url2 = $value["url"];
             $response2 = file_get_contents($url2);
         
@@ -46,28 +44,19 @@ $data = json_decode($response , true);
             タイプ：<?= $data2["types"][0]["type"]["name"]; ?><br>
             身長：<?= $data2["height"]; ?><br>
             体重：<?= $data2["weight"]; ?><br>
-        <?php } 
+            
+        <?php } ?>
+        <form action="" method="get">
+            <?php
         
-        echo '全件数'. $poke_num. '件'. '　'; // 全データ数の表示です。
- 
-        if($now > 1){ // リンクをつけるかの判定
-            echo '<a href="peke2.php?page_id='.($now - 1).'")>前へ</a>'. '　';
-        } else {
-            echo '前へ'. '　';
-        }
-        
-        for($i = 1; $i <= $max_page; $i++){
-            if ($i == $now) {
-                echo $now. '　'; 
-            } else {
-                echo '<a href="peke2.php?page_id='. $i. '")>'. $i. '</a>'. '　';
-            }
-        }
-        
-        if($now < $max_page){ // リンクをつけるかの判定
-            echo '<a href="/paging.php?page_id='.($now + 1).'")>次へ</a>'. '　';
-        } else {
-            echo '次へ';
-        }?>
+                if($offset != 0){ //リンクをつけるか判定 ?>
+                    <input type="submit" name="page" value="前へ">
+                <?php } else {
+                    echo '前へ'. '　';  
+                } ?>
+                    <input type="hidden" value="<?= $offset ?>" name="back_off">
+                    <input type="submit" name="page" value="次へ">
+        </form>
+
 </body>
 </html>
